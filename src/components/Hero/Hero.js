d650766a9f7dc9cc2927a4a61b987f04186a4d1f
Marian Ilie <ilie.marian32@yahoo.com>
2022-03-13 17:06:06 +0000

    about me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -20,15 +20,24 @@ const Hero = () => (
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        Welcome to Hollie Arnett Design and welcome to my portfolio! It is such
+        a pleasure to have you here and an honour to introduce my work to you,
+        so thank you for reading! I have spent the last seven years, throughout
+        high school and university and my own freelance career, creating various
+        pieces of work and building up my portfolio to where it is today;
+        updating my display of projects as my skills have grown and developed. I
+        hope you enjoy it! As I introduce my portfolio and you explore it for
+        yourself if you want to, you will notice that I have worked with a lot
+        of design mediums, exploring both print and web projects, and working in
+        areas from branding to user interfaces to advertising and even more. I
+        love how design can be used in so many different formats and mediums to
+        portray similar content in various ways in order to provide a unique
+        user experience, and I am super interested in the way that print and web
+        can work together in cool ways, so I am always flicking between them and
+        intertwining them together! Within my portfolio I also include work that
+        has been created for various purposes, whether it be from university
+        projects, commissioned freelance work, volunteer design or personal
+        pieces of work, so you will see lots of different types of work is
       </SectionText>
       <Button onClick={() => (window.location = "https://google.com")}>
         Learn More
