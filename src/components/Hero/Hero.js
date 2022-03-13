dfb130181ecc230b6402105784f55fd17a369afe
Marian Ilie <ilie.marian32@yahoo.com>
2022-03-13 20:26:37 +0000

    about me

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -16,28 +16,15 @@ const Hero = () => (
     <LeftSection>
       {/* SectionTitle props are -> main and center */}
       <SectionTitle main center>
-        Welcome To <br />
+        {/* Welcome To <br /> */}
         My Personal Portfolio
       </SectionTitle>
       <SectionText>
-        Welcome to Hollie Arnett Design and welcome to my portfolio! It is such
-        a pleasure to have you here and an honour to introduce my work to you,
-        so thank you for reading! I have spent the last seven years, throughout
-        high school and university and my own freelance career, creating various
-        pieces of work and building up my portfolio to where it is today;
-        updating my display of projects as my skills have grown and developed. I
-        hope you enjoy it! As I introduce my portfolio and you explore it for
-        yourself if you want to, you will notice that I have worked with a lot
-        of design mediums, exploring both print and web projects, and working in
-        areas from branding to user interfaces to advertising and even more. I
-        love how design can be used in so many different formats and mediums to
-        portray similar content in various ways in order to provide a unique
-        user experience, and I am super interested in the way that print and web
-        can work together in cool ways, so I am always flicking between them and
-        intertwining them together! Within my portfolio I also include work that
-        has been created for various purposes, whether it be from university
-        projects, commissioned freelance work, volunteer design or personal
-        pieces of work, so you will see lots of different types of work is
+        Welcome to my portfolio! It is such a pleasure to have you here and an
+        honour to introduce my work to you! I have spent the last three months,
+        throughout School of Code, creating various pieces of work and building
+        up my portfolio to where it is today; updating my display of projects as
+        my skills have grown and developed. I hope you enjoy it! 
       </SectionText>
       <Button onClick={() => (window.location = "https://google.com")}>
         Learn More
--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -78,7 +78,36 @@ const Timeline = () => {
   return (
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
-      <SectionText>how do i start learning ,my milestones,etc</SectionText>
+      <SectionText>
+        I'm a open-minded, agile, and creative person who started coding to
+        discover and build new great things. I can pick up complex concepts
+        quickly and I'm a highly motivated, self-driven learner, currently
+        completing a 3-month full-stack web development Bootcamp with the School
+        of Code. My current language is JavaScript.I have gained experience
+        working remotely in teams of great diversity, having worked on a
+        week-long project using agile methodologies. As well as this, I have
+        experience practicing test-driven development, using Jest and React
+        Testing Library for unit testing, Supertest, and Postman for API
+        testing, and Cypress for end-to-end testing. I have extensive experience
+        in React.js and particularly enjoy working in this area, using
+        libraries, tools, and frameworks such as Styled-Components, Next.js,
+        Auth0, Storybook, and Netlify for deployment. Additionally, I've
+        implemented multiple back-end (RESTful) solutions, creating APIs with
+        the Express.js library using Node.js. I've created many databases using
+        PostgreSQL and SQL++, deploying on Heroku and Couchbase respectively.
+        Other tools I have used include; styling using CSS modules, Git for
+        version control, Figma for designing wireframes, VS Code as an IDE, and
+        Slack, Zoom, and Microsoft Teams as communication channels. If you would
+        like to see some of my work and find out a bit more about me, please
+        visit: https://github.com/Ilie1985 or click the 'Portfolio' link:
+        https://marians-portfolio.netlify.app. In previous roles, I have shown
+        my ability to be customer-focused, to work under pressure to meet
+        targets, and to communicate with co-workers of all backgrounds and
+        abilities.. Overall, I have effective communication skills, love to
+        learn, and have a positive outlook. I like spending time , computational
+        thinking comes naturally to me. I work efficiently in a team using agile
+        framework and iterative approach to develop, test and deploy software.
+      </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         {/* loop over the time line data */}
         {/* create an empty react fragment */}
